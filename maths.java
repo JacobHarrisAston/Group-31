3201bac81c2f274fc22399239ff3d8bcf00579d5
Jacob Harris <116261314+JacobHarrisAston@users.noreply.github.com>
2022-10-20 11:31:44 +0100

    main class
    yeah

---
+++ maths.java
@@ -0,0 +1,12 @@
+
+public class maths {
+
+	public int power(int base, int exponent) {
+		return base^exponent;
+	}
+	
+	public static void main(String[] args) {
+		maths m = new maths();
+		System.out.println(m.power(2, 3));
+	}
+}
